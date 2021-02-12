d9c54498201c323c27ed9485e3c7d5e3c5ae8c21
developerOmega <developerOmega@developerOmega.com>
2021-02-12 13:52:16 -0600

    Estilos de header y footer

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import RouterIndex from './routes/Index';
 import fileUpload from 'express-fileupload';
 import { port as portLog } from './config/config';
 import bodyParser = require('body-parser');
+import cors = require('cors');
 
 const port:any = portLog;
 const server = Server.init(port);
@@ -11,6 +12,7 @@ server.app.use(bodyParser.urlencoded({ extended: false }));
 server.app.use(bodyParser.json());
 
 server.app.use(fileUpload({ useTempFiles: false }));
+server.app.use(cors());
 
 server.app.use(RouterIndex);
 
